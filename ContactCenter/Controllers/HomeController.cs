31f8ce17dc837a32d0ab5758a70e9a6a56ae7a12
Thomas Curtis <thomas.e.curtis@gmail.com>
2018-03-16 19:53:47 +0000

    Contact-Centre-C#

---
+++ ContactCenter/Controllers/HomeController.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Configuration;
+using System.Linq.Expressions;
+using Twilio.Rest.Taskrouter.V1.Workspace;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account.Conference;
+using Twilio.TwiML;
+using Twilio.TwiML.Voice;
+using Twilio.Jwt.Taskrouter;
+using Twilio.Jwt.Client;
+using Twilio.Http;
+using Twilio.Jwt;
+
+namespace ContactCenter.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        protected string _accountSid = ConfigurationManager.AppSettings["TWILIO_ACME_ACCOUNT_SID"];
+        protected string  _authToken = ConfigurationManager.AppSettings["TWILIO_ACME_AUTH_TOKEN"];
+        protected string _applicationSid =  ConfigurationManager.AppSettings["TWILIO_ACME_TWIML_APP_SID"];
+        protected string _workspaceSid = ConfigurationManager.AppSettings["TWILIO_ACME_WORKSPACE_SID"];
+        protected string _manager_workflow = ConfigurationManager.AppSettings["TWILIO_ACME_MANAGER_WORKFLOW_SID"];
+        protected string _support_workflow = ConfigurationManager.AppSettings["TWILIO_ACME_SUPPORT_WORKFLOW_SID"];
+        protected string _sales_workflow = ConfigurationManager.AppSettings["TWILIO_ACME_SALES_WORKFLOW_SID"];
+        protected string _billing_workflow = ConfigurationManager.AppSettings["TWILIO_ACME_BILLING_WORKFLOW_SID"];
+        protected string _called_id = ConfigurationManager.AppSettings["TWILIO_ACME_CALLERID"];
+       
+      
+        class PolicyUrlUtils
+        {
+            const string taskRouterBaseUrl = "https://taskrouter.twilio.com";
+            const string taskRouterVersion = "v1";
+
+            readonly string _workspaceSid;
+            readonly string _workerSid;
+
+            public PolicyUrlUtils(string workspaceSid, string workerSid)
+            {
+                _workspaceSid = workspaceSid;
+                _workerSid = workerSid;
+            }
+
+            public string AllTasks => $"{Workspace}/Tasks/**";
+            public string Worker => $"{Workspace}/Workers/{_workerSid}";
+            public string AllReservations => $"{Worker}/Reservations/**";
+            public string Workspace =>
+                $"{taskRouterBaseUrl}/{taskRouterVersion}/Workspaces/{_workspaceSid}";
+            public string Activities => $"{Workspace}/Activities";
+
+        }
+        
+      
+        
+        public ActionResult Index()
+       
+        {
+             
+            
+            return View();
+        }
+
+        public ActionResult Incoming_call()
+        {
+            var action = new Uri("/Home/ProcessDigits");
+            var response = new VoiceResponse();
+            var gather = new Gather(timeout: 3, numDigits: 1, action: action);
+            gather.Say("Welcome to ACME corp, please select your department");
+            gather.Say("For Sales press one, for Support press two, for billing press three", language: "en-gb");
+
+            response.Append(gather);
+            
+           
+            return Content(response.ToString(), contentType: "text/xml");
+        }
+
+        public ActionResult ProcessDigits()
+
+        {
+            var response = new VoiceResponse();
+            
+            Dictionary<string, string> department = new Dictionary<string, string>();
+            department.Add("1", "sales");
+            department.Add("2", "support");
+            department.Add("3", "billing");
+
+            Dictionary<string, string> workflowDictionary = new Dictionary<string, string>();
+            workflowDictionary.Add("1", _sales_workflow);
+            workflowDictionary.Add("2", _support_workflow);
+            workflowDictionary.Add("3", _billing_workflow);
+
+            var enqueue = new Enqueue(workflowSid:workflowDictionary[Request.Params.Get("digits")]);
+
+            enqueue.Task("{'selected_product:'@" + department[Request.Params.Get("digits")] + @"'}");
+            
+              
+            response.Append(enqueue);
+            return Content(response.ToString(), contentType: "text/xml");
+            
+        }
+
+        public ActionResult Agent_list()
+        {
+
+            return View();
+            
+        }
+
+        public ActionResult Chat()
+        {
+
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult CallTransfer()
+        {
+           
+            TwilioClient.Init(_accountSid, _authToken);
+        
+        
+                    string conferenceSid = Request.Params.Get("conference");
+                    string callSid = Request.Params.Get("participant");
+                    ParticipantResource.Update(
+                        conferenceSid,
+                        callSid,
+                        hold: true
+                    );    
+
+            try
+            {
+
+                string json = @"{
+               'selected_product': 'manager',
+              'conference': '" + Request.Params.Get("conference") + @"',
+              'customer': '" + Request.Params.Get("participant") + @"',
+              'customer_taskSid': '" + Request.Params.Get("taskSid") + @"',
+              'from': '" + Request.Params.Get("from") + @"',
+               }";
+
+                var task = TaskResource.Create(
+                    _workspaceSid, attributes: JsonConvert.DeserializeObject(json).ToString(),
+                    workflowSid: _manager_workflow
+                );
+
+            }
+            catch (Exception e)
+            {
+                
+                Console.Write(e.ToString());
+            }
+        
+        return View();
+        }
+
+        public ActionResult CallMute()
+        {
+            
+            TwilioClient.Init(_accountSid, _authToken);
+
+                string conferenceSid = Request.Params.Get("conference");
+                string callSid = Request.Params.Get("participant");
+                bool muted = Convert.ToBoolean(Request.Params.Get("muted"));
+            
+                ParticipantResource.Update(
+                    conferenceSid,
+                    callSid,
+                    hold: muted
+                );
+
+       
+            var response = new VoiceResponse();
+          
+            return Content(response.ToString(), contentType: "text/xml");
+        }
+
+        public ActionResult TransferTwiml()
+        {
+
+            var response = new VoiceResponse();
+            var  dial = new Dial();
+            dial.Conference(Request.Params.Get("conference"));
+
+            response.Append(dial);
+            
+            Console.Write(response.ToString());
+            
+            return Content(response.ToString(), contentType: "text/xml");
+        }
+       
+        public ActionResult Agent_desktop()
+        {
+
+            string workerSid = Request.Params.Get("WorkerSid");
+            TwilioClient.Init(_accountSid, _authToken);
+
+            var activityDictionary = new Dictionary<string, string>();
+            
+            
+            var activities = ActivityResource.Read(_workspaceSid);
+            foreach(var activity in activities) {
+                activityDictionary.Add(activity.FriendlyName, activity.Sid);
+                
+            }
+            
+            
+            var updateActivityFilter = new Dictionary<string, Policy.FilterRequirement>
+            {
+                { "ActivitySid", Policy.FilterRequirement.Required }
+            };
+
+            var urls = new PolicyUrlUtils(_workspaceSid, workerSid);
+
+            var allowActivityUpdates = new Policy(urls.Worker,
+                HttpMethod.Post,
+                postFilter: updateActivityFilter);
+            var allowTasksUpdate = new Policy(urls.AllTasks, HttpMethod.Post);
+            var allowReservationUpdate = new Policy(urls.AllReservations, HttpMethod.Post);
+            var allowWorkerFetches = new Policy(urls.Worker, HttpMethod.Get);
+            var allowTasksFetches = new Policy(urls.AllTasks, HttpMethod.Get );
+            var allowReservationFetches = new Policy(urls.AllReservations, HttpMethod.Get);
+            var allowActivityFetches = new Policy(urls.Activities, HttpMethod.Get);
+
+            var policies = new List<Policy>
+            {
+                allowActivityUpdates,
+                allowTasksUpdate,
+                allowReservationUpdate,
+                allowWorkerFetches,
+                allowTasksFetches,
+                allowReservationFetches
+
+            };
+
+            var capability = new TaskRouterCapability(
+                _accountSid,
+                _authToken,
+                _workspaceSid,
+                workerSid,
+                policies: policies); 
+
+            var workerToken = capability.ToJwt();
+            
+            var scopes = new HashSet<IScope>
+            {
+                { new IncomingClientScope(Request.Params.Get("WorkerSid")) },
+                { new OutgoingClientScope(_applicationSid) }
+            };
+            
+            var webClientCapability = new ClientCapability(_accountSid, _authToken, scopes: scopes);
+            var token = capability.ToJwt();
+            
+            ViewBag.worker_token = workerToken;
+            ViewBag.client_token = webClientCapability.ToJwt();
+            ViewBag.caller_ID = _called_id;
+            ViewBag.activties = activityDictionary;
+            return View();
+
+        }
+    }
+}
---
+++ ContactCenter/Scripts/customer_chat.js
@@ -0,0 +1,141 @@
+$(function() 
+{
+    // Get handle to the chat div 
+    var $chatWindow = $('#messages');
+
+    // Our interface to the Chat service
+    var chatClient;
+
+    // A handle to the "general" chat channel - the one and only channel we
+    // will have in this sample app
+    var generalChannel;
+
+    // The server will assign the client a random username - store that value
+    // here
+    var username;
+
+
+    function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    // Helper function to print info messages to the chat window
+    function print(infoMessage, asHtml) {
+        var $msg = $('<div class="info">');
+        if (asHtml) {
+            $msg.html(infoMessage);
+        } else {
+            $msg.text(infoMessage);
+        }
+        $chatWindow.append($msg);
+    }
+
+    // Helper function to print chat message to the chat window
+    function printMessage(fromUser, message) {
+        var $user = $('<span class="username">').text(fromUser + ':');
+        if (fromUser === username) {
+            $user.addClass('me');
+        }
+        var $message = $('<span class="message">').text(message);
+        var $container = $('<div class="message-container">');
+        $container.append($user).append($message);
+        $chatWindow.append($container);
+        $chatWindow.scrollTop($chatWindow[0].scrollHeight);
+    }
+
+    // Alert the user they have been assigned a random username
+    //print('Logging in...');
+
+    // Get an access token for the current user, passing a username (identity)
+    // and a device ID - for browser-based apps, we'll always just use the 
+    // value "browser"
+    $.getJSON('/token', {
+        identity: 'Customer',
+        device: 'browser',
+        logLevel: 'debug'
+    }, function(data) {
+        // Alert the user they have been assigned a random username
+        username = 'Customer'
+        //print('You have been assigned a random username of: '
+          //  + '<span class="me">' + username + '</span>', true);
+
+        // Initialize the Chat client
+        chatClient = new Twilio.Chat.Client(data.token);
+        chatClient.getSubscribedChannels().then(createOrJoinGeneralChannel);
+
+    });
+    function createOrJoinGeneralChannel() {
+              var channelName = getRandomArbitrary(0,10000000);
+       //    print('Setting up  ' + channelName + ' chat channel...');
+                var promise = chatClient.getChannelByUniqueName(channelName);
+                promise.then(function(channel) {
+                    generalChannel = channel;
+                    console.log('Found ' + channel.friendlyName + ' channel:');
+                    console.log(generalChannel);
+
+                    setupChannel();
+                }).catch(function() {
+                    // If it doesn't exist, let's create it
+                    console.log('Creating general channel');
+                    chatClient.createChannel({
+                        uniqueName: channelName,
+                        friendlyName: 'Customer request ' + channelName,
+                    }).then(function(channel) {
+                        console.log('Created +' + channelName + +' channel:');
+                        console.log(channel);
+                        generalChannel = channel;
+                        setupChannel();
+
+
+                    });
+                });
+
+           // });
+
+    }
+
+    // Set up channel after it has been found
+    function setupChannel() {
+        // Join the general channel
+
+        generalChannel.join().then(function(channel) {
+            print('<span class="me">Welcome ' + username + ', please wait for an agent to join you.</span>.', true);
+              $.post("/createChatTask/", {
+                channel: channel.sid,
+            });
+
+        });
+
+          // Listen for new messages sent to the channel
+        generalChannel.on('messageAdded', function(message) {
+            printMessage(message.author, message.body);
+        });
+
+        generalChannel.on('typingStarted', function (member) {
+            document.getElementById("typing").innerText =  member.identity + " is typing";
+
+            });
+
+        generalChannel.on('typingEnded', function (member) {
+
+          document.getElementById("typing").innerText =  member.identity + " stopped typing";
+
+          //  printMessage(member.identity + ' has stopped typing');
+             console.log(" no sponge");
+        });
+    }
+            // Send a new message to the general channel
+                var $input = $('#chat-input');
+                $input.on('keydown', function(e) {
+                    if (e.keyCode == 13 && generalChannel) {
+                        generalChannel.sendMessage($input.val())
+                        $input.val('');
+                        console.log("input");
+                    }
+                    else {
+                // else send the Typing Indicator signal
+
+                        generalChannel.typing();
+                    }
+                });
+});
+
